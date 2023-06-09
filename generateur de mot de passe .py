0cb75cca6db5b42e10eb731bc9420d971da1f533
thib <115115143+ThibThibThib@users.noreply.github.com>
2023-06-09 17:21:13 +0200

    Add files via upload

---
+++ generateur de mot de passe .py
@@ -0,0 +1,74 @@
+import random 
+
+malist = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","1","2","3","4","5","6","7","8","9",",","!","/","'","é","&","-","(",")"]
+
+
+rand = random.choice(malist)
+rand2 = random.choice(malist)
+rand3 = random.choice(malist)
+rand4 = random.choice(malist)
+rand5 = random.choice(malist)
+rand6 = random.choice(malist)
+rand6 = random.choice(malist)
+rand7 = random.choice(malist)
+rand8 = random.choice(malist)
+rand9 = random.choice(malist)
+rand10 = random.choice(malist)
+rand11 = random.choice(malist)
+rand12 = random.choice(malist)
+rand13 = random.choice(malist)
+rand14 = random.choice(malist)
+rand15 = random.choice(malist)
+rand16 = random.choice(malist)
+rand17 = random.choice(malist)
+rand18 = random.choice(malist)
+
+
+
+print("Combien voulez vous de caractère dans votre mot de passe ? Veuillez saisir un , deux , trois , quatre , cinq , six , sept , huit , neuf , dix , onze , douze , treize , quatorze , quinze , seize , dix-sept , dix-huit")
+reponse = input()
+
+
+if reponse == "un" : 
+    print("Voici votre mot de passe :", rand)
+elif reponse == 'unun' :
+    print(rand,rand)
+elif reponse == "deux" :
+    print("Voici votre mot de passe :",rand,rand2)
+elif reponse == "trois" : 
+    print("Voici votre mot de passe :", rand,rand2,rand3)
+elif reponse == "quatre" :
+    print("Voici votre mot de passe :", rand,rand2,rand3,rand4)
+elif reponse == "cinq" :
+    print("Voici votre mot de passe :", rand,rand2,rand3,rand4 ,rand5)
+elif reponse == "six" :
+    print("Voici votre mot de passe :", rand,rand2,rand3,rand4,rand5,rand6)
+elif reponse == "sept" :
+    print("Voici votre mot de passe :", rand,rand2,rand3,rand4,rand5,rand6,rand7)
+elif reponse == "huit" : 
+    print("Voici votre mot de passe :", rand,rand2,rand3,rand4,rand5,rand6,rand7,rand8)
+elif reponse == "neuf" :
+    print("Voici votre mot de passe :", rand,rand2,rand3,rand4,rand5,rand6,rand7,rand8,rand9)
+elif reponse == "dix" :
+    print("Voici votre mot de passe :", rand,rand2,rand3,rand4,rand5,rand6,rand7,rand8,rand9,rand10)
+elif reponse == "onze" :
+    print("Voici votre mot de passe :", rand,rand2,rand3,rand4,rand5,rand6,rand7,rand8,rand9,rand10,rand11)
+elif reponse == "douze" :
+    print("Voici votre mot de passe :", rand,rand2,rand3,rand4,rand5,rand6,rand7,rand8,rand9,rand10,rand11,rand12)
+elif reponse == "treize" :
+    print("Voici votre mot de passe :", rand,rand2,rand3,rand4,rand5,rand6,rand7,rand8,rand9,rand10,rand11,rand12,rand13)
+elif reponse == "quatorze" :
+    print("Voici votre mot de passe :", rand,rand2,rand3,rand4,rand5,rand6,rand7,rand8,rand9,rand10,rand11,rand12,rand13,rand14)
+elif reponse == "quinze" :
+    print("Voici votre mot de passe :", rand,rand2,rand3,rand4,rand5,rand6,rand7,rand8,rand9,rand10,rand11,rand12,rand13,rand14,rand15)
+elif reponse == "seize" :
+    print("Voici votre mot de passe :", rand,rand2,rand3,rand4,rand5,rand6,rand7,rand8,rand9,rand10,rand11,rand12,rand13,rand14,rand15,rand16)
+elif reponse == "dix-sept" or "dix sept" :
+    print("Voici votre mot de passe :", rand,rand2,rand3,rand4,rand5,rand6,rand7,rand8,rand9,rand10,rand11,rand12,rand13,rand14,rand15,rand16,rand17,)
+elif reponse == "dix-huit" or "dix huit" :
+    print("Voici votre mot de passe :", rand,rand2,rand3,rand4,rand5,rand6,rand7,rand8,rand9,rand10,rand11,rand12,rand13,rand14,rand15,rand16,rand17,rand18)
+
+
+
+
+
